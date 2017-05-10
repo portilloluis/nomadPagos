0be5563c85f1ea87dc11b781908a192eacb08a87
portilloluis <lcpdiseno@gmail.com>
2017-05-10 16:39:07 -0500

    Función de activar imagen al momento de dar click.

--- js/pagos.js
+++ js/pagos.js
@@ -1,9 +1,10 @@
 $(".ico").click(function(){
     var contenedor = $(this).parent();
     var imagenes = contenedor.children();
-     imagenes.each(function(){
+    imagenes.each(function(){
          if ($(this).hasClass("active")){
              $(this).removeClass("active")
          }
-     })
+    })
+    $(this).addClass("active");
 })
