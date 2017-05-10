8625bf026b671605b3cbcbb65123dfd1e16ce369
portilloluis <lcpdiseno@gmail.com>
2017-05-10 16:26:57 -0500

    Ajustes en el css del modal

---
+++ js/pagos.js
@@ -0,0 +1,9 @@
+$(".ico").click(function(){
+    var contenedor = $(this).parent();
+    var imagenes = contenedor.children();
+     imagenes.each(function(){
+         if ($(this).hasClass("active")){
+             $(this).removeClass("active")
+         }
+     })
+})
